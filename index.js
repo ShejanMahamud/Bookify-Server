1b5a8285c12a4497618efb87f921ef127751bc5b
ShejanMahamud <mdshejanmahamud006@gmail.com>
2024-05-09 20:33:21 +0600

    Project Setup Complete

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 4549;
+const mongoURI = process.env.MONGO_URI;
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const secret_token = process.env.ACCESS_TOKEN_SECRET;
+
+//app
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173/"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+//mongo client
+const client = new MongoClient(mongoURI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    // await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+};
+
+run().catch((error) => console.log);
+
+app.get("/", (req, res) => {
+  res.send({ server_status: "Server Running" });
+});
+
+app.listen(port, () => {
+  console.log("Server Running On", port);
+});
