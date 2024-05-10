3828db86abb8e04e4ade4e5300b93d416caefca9
ShejanMahamud <mdshejanmahamud006@gmail.com>
2024-05-10 18:13:52 +0600

    Added User Role

--- index.js
+++ index.js
@@ -12,13 +12,17 @@ const secret_token = process.env.ACCESS_TOKEN_SECRET;
 const app = express();
 
 //middlewares
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://bookify-library.netlify.app',
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["http://localhost:5173/"],
-    credentials: true,
-  })
-);
 app.use(cookieParser());
 
 //mongo client
@@ -33,11 +37,23 @@ const client = new MongoClient(mongoURI, {
 const run = async () => {
   try {
     // await client.connect();
+    const usersCollection = client.db('bookify').collection('users');
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    app.post('/users',async(req,res)=>{
+      const user = req.body;
+      const result = await usersCollection.insertOne(user)
+      res.send(result)
+    })
+
+    app.get('/users',async(req,res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
   }
 };
